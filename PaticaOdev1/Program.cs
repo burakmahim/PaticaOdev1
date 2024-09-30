be946dc145350b8c075e48c088d578d3d608b674
burakmahim <burakmahim718@gmail.com>
2024-09-30 13:30:29 +0300

    Proje dosyası ekle.

---
+++ PaticaOdev1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Burak MAHİM");
+            Console.WriteLine("Bu güne kadar hep frontend üzerine çalıştım ve piyasada sadece frontend üzerine iş bulnak çok zor bende yanına  sektörde çok fazla kullanılan .NET eklemek istedim frontendi de zaten patika ve kodluyoruzdan öğrenmiştim.");
+        }
+    }
+}
